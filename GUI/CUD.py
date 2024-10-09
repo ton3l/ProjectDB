8c818063a6136b20f45b6d66aeaeaf0f40d3d6aa
ELTON FELYPE DA SILVA NASCIMENTO <20221214010016@ifrn.local>
2024-10-09 14:24:15 -0300

    Trabalhando na tela CUD, organizando lista de nomes e botões

--- GUI/CUD.py
+++ GUI/CUD.py
@@ -7,16 +7,20 @@ class CUDScreen:
         ROOT.geometry("200x350");
 
         SCROLL = Scrollbar(ROOT);
-        LB = Listbox(ROOT, yscrollcommand=SCROLL.set);
+        LB = Listbox(ROOT, yscrollcommand=SCROLL.set, font=('Arial', ), width=10);
 
         SCROLL.pack(side=RIGHT, fill=Y)
-        LB.pack(side=LEFT, fill=BOTH)
+        LB.pack(side=LEFT, fill=Y)
+        
+
 
         usbd = UsuarioBanco()
         users = usbd.selectAll()
 
         for user in users:
             LB.insert(END, user[0])
+            BUTTON = Button(ROOT, text="edit", height=1)
+            BUTTON.pack(side=TOP)
 
         SCROLL.configure(command=LB.yview)
 
